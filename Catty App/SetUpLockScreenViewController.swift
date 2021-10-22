aac05ea420a5d832b9891f0de54764a481798d8a
Kate Ulasik <>
2021-10-22 19:06:49 +0300

    added new password screeen

--- Catty App/SetUpLockScreenViewController.swift
+++ Catty App/SetUpLockScreenViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 struct Constants {
-    static let savePasswordText = "this is password"
+    static let savePasswordKey = "this is password"
 }
 
 class SetUpLockScreenViewController: UIViewController {
@@ -28,6 +28,18 @@ class SetUpLockScreenViewController: UIViewController {
         contentViewTapGestureRecognizer.addTarget(self, action: #selector(contentViewDidTap(_:)))
         
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let _ = recover() {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .red
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+    }
+    
     @objc func keyboardWillShow(_ notification: NSNotification) {
         
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {return}
@@ -54,13 +66,30 @@ class SetUpLockScreenViewController: UIViewController {
         let password = passwordTextField.text
         
         if let NotOptionalPassword = password, NotOptionalPassword.count == 6 {
-            UserDefaults.standard.set(password, forKey: "this is password")
+           save(password: password)
+        
+            showAlert(title: "Perfect", message: "Password saved succesfuly")
         } else {
-            passwordTextField.text = "Password is wrong"
+            showAlert(title: "ERROR", message: "Password is wrong!")
         }
     }
-    @IBAction func recoverPassword(_ sender: Any) {
-      let password = UserDefaults.standard.string(forKey: "this is password")
-        passwordTextField.text = "Recovered password is \(String(describing: password))"
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let closeAction = UIAlertAction(title: "Close", style: .cancel, handler: nil)
+        
+        alert.addAction(closeAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func save(password: String?) {
+        UserDefaults.standard.set(password, forKey: Constants.savePasswordKey)
+    }
+    
+    private func recover () -> String? {
+        let password = UserDefaults.standard.string(forKey: Constants.savePasswordKey)
+        return password
     }
 }
+
