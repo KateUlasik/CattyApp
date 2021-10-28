4ddf4bfb92fb780e8c63b23260e6470217d95d99
mby4bitbucket <mby@tut.by>
2021-10-28 18:31:38 +0300

    Added password saving

--- Catty App/PasswordViewController/PasswordViewController.swift
+++ Catty App/PasswordViewController/PasswordViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class PasswordViewController: UIViewController {
+    var currentPassword: String = ""
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,4 +30,24 @@ class PasswordViewController: UIViewController {
     }
     */
 
+    @IBAction func buttonDidTap(_ sender: Any) {
+        if let button = sender as? UIButton, button == button1 {
+            currentPassword = currentPassword + "1"
+        } else if let button = sender as? UIButton, button == button2 {
+            currentPassword = currentPassword + "2"
+        }
+        
+        if currentPassword.count == 6 {
+            let recoveredPassword = UserDefaults.standard.string(forKey: Constants.savePasswordKey)
+            
+            if let password = recoveredPassword, currentPassword == password {
+                print("Success!")
+            } else {
+                print("Wrong!")
+            }
+            
+            currentPassword = ""
+        }
+    }
+    
 }
